636cd6572a5e29b8b2060d9e0515cd0b8fd1941a
Even <yh821@qq.com>
2018-06-06 16:04:36 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿using System.Windows.Forms;
+
+namespace PictureToByte
+{
+    partial class Form1
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要使用代码编辑器修改
+        /// 此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(660, 495);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开图片";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(330, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 22);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "保存byte.txt";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(224, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 22);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "打开byte.txt";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(436, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 22);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "打开Base64.txt";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(542, 35);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 22);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "保存Base64.txt";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 35);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 22);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "保存图片";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(158, 15);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(60, 16);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "D3格式";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(589, 555);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "尺寸:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(501, 555);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "类型:";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(118, 35);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 22);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "图片另存为";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(330, 12);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 22);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "另存byte.txt";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(542, 12);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(100, 22);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "另存Base64.txt";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(224, 35);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(100, 22);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "打开txt路径";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(436, 35);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(100, 22);
+            this.button11.TabIndex = 14;
+            this.button11.Text = "打开img路径";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleBaseSize = new System.Drawing.Size(6, 14);
+            this.ClientSize = new System.Drawing.Size(685, 570);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private CheckBox checkBox1;
+        private Label label1;
+        private Label label2;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PictureToByte
+{
+    public partial class Form1 : Form
+    {
+        private byte[] mBuffer;
+        private Size mBufferSize;
+        private string FileName;
+
+        /// <summary>
+        /// 打开图片
+        /// </summary>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog of = new OpenFileDialog();
+            of.Filter = "Image|*.png;*.jpg;*.bmp";
+            of.Title = "图片路径";
+
+            if (of.ShowDialog() == DialogResult.OK && !string.IsNullOrEmpty(of.FileName))
+            {
+                try
+                {
+                    FileName = Path.GetFileName(of.FileName);
+                    FileName = FileName.Substring(0, FileName.LastIndexOf('.'));
+                    Image im = Image.FromFile(of.FileName);
+                    pictureBox1.Image = im;
+                    mBufferSize = im.Size;
+                    label1.Text = $"尺寸:{mBufferSize.Width}x{mBufferSize.Height}";
+                    label2.Text = $"类型:{ImageFormatToString(pictureBox1.Image)}";
+
+                    MemoryStream ms = new MemoryStream();
+                    im.Save(ms, im.RawFormat);
+                    mBuffer = ms.GetBuffer();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "打开图片失败!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 导出byte
+        /// </summary>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("未导入图片！");
+                return;
+            }
+
+            try
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < mBuffer.Length; i++)
+                {
+                    if (checkBox1.Checked)
+                    {
+                        if (i == 0)
+                            sb.Append(mBuffer[i].ToString("D3"));
+                        else if (i % mBufferSize.Width == 0)
+                            sb.Append(',').Append("\r\n").Append(mBuffer[i].ToString("D3"));
+                        else
+                            sb.Append(',').Append(mBuffer[i].ToString("D3"));
+                    }
+                    else
+                    {
+                        if (i == 0)
+                            sb.Append(mBuffer[i]);
+                        else if (i % mBufferSize.Width == 0)
+                            sb.Append(',').Append("\r\n").Append(mBuffer[i]);
+                        else
+                            sb.Append(',').Append(mBuffer[i]);
+                    }
+                }
+
+                string path = $"{Application.StartupPath}\\txt";
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+                path = $"{path}\\{FileName}_byte.txt";
+
+                StreamWriter sw = new StreamWriter(path);
+                sw.WriteLine(sb.ToString());
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "导出txt文本失败!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        /// <summary>
+        /// 导入byte
+        /// </summary>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog of = new OpenFileDialog();
+            of.Filter = "Text Document(*.txt)|*.txt";
+            of.Title = "选择要导入的txt文本路径";
+
+            if (of.ShowDialog() == DialogResult.OK && !string.IsNullOrEmpty(of.FileName))
+            {
+                try
+                {
+                    FileName = Path.GetFileName(of.FileName);
+                    FileName = FileName.Substring(0, FileName.LastIndexOf('.'));
+                    StreamReader sr = new StreamReader(of.FileName, System.Text.Encoding.UTF8, true, 128);
+                    string[] strBytes = sr.ReadToEnd().Split(',');
+                    sr.Close();
+                    byte[] bytes = new byte[strBytes.Length];
+                    for (int i = 0; i < bytes.Length; i++)
+                        bytes[i] = (byte)int.Parse(strBytes[i]);
+                    mBuffer = bytes;
+                    MemoryStream ms = new MemoryStream(bytes);
+                    pictureBox1.Image = new Bitmap(ms);
+                    mBufferSize = pictureBox1.Image.Size;
+                    label1.Text = $"尺寸:{mBufferSize.Width}x{mBufferSize.Height}";
+                    label2.Text = $"类型:{ImageFormatToString(pictureBox1.Image)}";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "导入txt文本失败!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 导出base64
+        /// </summary>
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("未导入图片！");
+                return;
+            }
+
+            try
+            {
+                string path = $"{Application.StartupPath}\\txt";
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+                path = $"{path}\\{FileName}_base64.txt";
+
+                StreamWriter sw = new StreamWriter(path, false, Encoding.UTF8, 128);
+                sw.WriteLine(Convert.ToBase64String(mBuffer));
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "导出txt文本失败!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        /// <summary>
+        /// 导入base64
+        /// </summary>
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog of = new OpenFileDialog();
+            of.Filter = "Text Document(*.txt)|*.txt";
+            of.Title = "选择要导入的txt文本路径";
+
+            if (of.ShowDialog() == DialogResult.OK && !string.IsNullOrEmpty(of.FileName))
+            {
+                try
+                {
+                    FileName = Path.GetFileName(of.FileName);
+                    FileName = FileName.Substring(0, FileName.LastIndexOf('.'));
+                    StreamReader sr = new StreamReader(of.FileName, Encoding.UTF8, true, 128);
+                    string utf8 = sr.ReadToEnd();
+                    sr.Close();
+                    MemoryStream ms = new MemoryStream(Convert.FromBase64String(utf8));
+                    pictureBox1.Image = new Bitmap(ms);
+                    mBufferSize = pictureBox1.Image.Size;
+                    label1.Text = $"尺寸:{mBufferSize.Width}x{mBufferSize.Height}";
+                    label2.Text = $"类型:{ImageFormatToString(pictureBox1.Image)}";
+
+                    ms = new MemoryStream();
+                    pictureBox1.Image.Save(ms, pictureBox1.Image.RawFormat);
+                    mBuffer = ms.GetBuffer();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "导入txt文本失败!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 保存图片
+        /// </summary>
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("未导入图片！");
+                return;
+            }
+            try
+            {
+                string path = $"{Application.StartupPath}\\img";
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+                pictureBox1.Image.Save($"{path}\\{FileName}.{ImageFormatToString(pictureBox1.Image)}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "保存图片失败!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private string ImageFormatToString(Image image)
+        {
+            if (image.RawFormat.Guid == ImageFormat.Tiff.Guid)
+                return "tiff";
+            if (image.RawFormat.Guid == ImageFormat.Gif.Guid)
+                return "gif";
+            if (image.RawFormat.Guid == ImageFormat.Jpeg.Guid)
+                return "jpg";
+            if (image.RawFormat.Guid == ImageFormat.Bmp.Guid)
+                return "bmp";
+            if (image.RawFormat.Guid == ImageFormat.Png.Guid)
+                return "png";
+            if (image.RawFormat.Guid == ImageFormat.Icon.Guid)
+                return "icon";
+
+            return "???";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("未导入图片！");
+                return;
+            }
+
+            SaveFileDialog sf = new SaveFileDialog();
+            sf.Filter = "Png Image|*.png|Jpeg Image|*.jpg|Bmp Image|*.bmp";
+            sf.Title = "选择保存的图片路径";
+
+            if (sf.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    FileStream fs = (FileStream)sf.OpenFile();
+                    switch (sf.FilterIndex)
+                    {
+                        case 1:
+                            pictureBox1.Image.Save(fs, ImageFormat.Png);
+                            break;
+
+                        case 2:
+                            pictureBox1.Image.Save(fs, ImageFormat.Jpeg);
+                            break;
+
+                        case 3:
+                            pictureBox1.Image.Save(fs, ImageFormat.Bmp);
+                            break;
+                    }
+                    fs.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "保存图片失败!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("未导入图片！");
+                return;
+            }
+
+            SaveFileDialog sf = new SaveFileDialog();
+            sf.Filter = "Text Document(*.txt)|*.txt";
+            sf.Title = "选择保存的txt文本路径";
+
+            if (sf.ShowDialog() == DialogResult.OK && !string.IsNullOrEmpty(sf.FileName))
+            {
+                try
+                {
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < mBuffer.Length; i++)
+                    {
+                        if (checkBox1.Checked)
+                        {
+                            if (i == 0)
+                                sb.Append(mBuffer[i].ToString("D3"));
+                            else if (i % mBufferSize.Width == 0)
+                                sb.Append("\r\n").Append(mBuffer[i].ToString("D3"));
+                            else
+                                sb.Append(',').Append(mBuffer[i].ToString("D3"));
+                        }
+                        else
+                        {
+                            if (i == 0)
+                                sb.Append(mBuffer[i]);
+                            else if (i % mBufferSize.Width == 0)
+                                sb.Append("\r\n").Append(mBuffer[i]);
+                            else
+                                sb.Append(',').Append(mBuffer[i]);
+                        }
+                    }
+
+                    StreamWriter sw = new StreamWriter(sf.FileName);
+                    sw.WriteLine(sb.ToString());
+                    sw.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "导出txt文本失败!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("未导入图片！");
+                return;
+            }
+
+            SaveFileDialog sf = new SaveFileDialog();
+            sf.Filter = "Text Document(*.txt)|*.txt";
+            sf.Title = "选择保存的txt文本路径";
+
+            if (sf.ShowDialog() == DialogResult.OK && !string.IsNullOrEmpty(sf.FileName))
+            {
+                try
+                {
+                    StreamWriter sw = new StreamWriter(sf.FileName, false, Encoding.UTF8, 128);
+                    sw.WriteLine(Convert.ToBase64String(mBuffer));
+                    sw.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "导出txt文本失败!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            string path = $"{Application.StartupPath}\\txt";
+            if (Directory.Exists(path))
+            {
+                System.Diagnostics.Process.Start(path);
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            string path = $"{Application.StartupPath}\\img";
+            if (Directory.Exists(path))
+            {
+                System.Diagnostics.Process.Start(path);
+            }
+        }
+    }
+}
